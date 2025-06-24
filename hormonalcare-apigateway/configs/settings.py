53c364987334dc44d153e18135910c41a773abc7
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-06-23 21:35:11 -0500

    fix: local database and server ports fixed

--- hormonalcare-apigateway/configs/settings.py
+++ hormonalcare-apigateway/configs/settings.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 # Configuración de microservicios
 MICROSERVICE_IAM = os.getenv(
-    "MICROSERVICE_IAM_URL", "http://localhost:8082/api/v1")
+    "MICROSERVICE_IAM_URL", "http://localhost:8081/api/v1")
 MICROSERVICE_MEDICAL_RECORD = os.getenv(
-    "MICROSERVICE_MEDICAL_RECORD_URL", "http://localhost:8083/api/v1/medical-records")
+    "MICROSERVICE_MEDICAL_RECORD_URL", "http://localhost:8082/api/v1/medical-records")
 MICROSERVICE_NOTIFICATION = os.getenv(
-    "MICROSERVICE_NOTIFICATION_URL", "http://localhost:8084/api/v1/notifications")
+    "MICROSERVICE_NOTIFICATION_URL", "http://localhost:8083/api/v1/notifications")
 MICROSERVICE_COMMUNICATION = os.getenv(
-    "MICROSERVICE_COMMUNICATION_URL", "http://localhost:8085/api/v1/communications")
+    "MICROSERVICE_COMMUNICATION_URL", "http://localhost:8084/api/v1/communications")
